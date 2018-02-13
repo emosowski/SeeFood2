520cf466c56a1ec2d3efc4f7847e39b8fa31c5c6
Emily Osowski <emosowski@gmail.com>
2018-02-13 07:17:46 -0600

    Implement image recognition using IBM Bluemix.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import VisualRecognitionV3
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let apiKey = "8d0ddf8f7a844296caae1da0d3d7afb64c3654ff"
+    let version = "2018-02-12"
     
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var imageView: UIImageView!
@@ -26,7 +30,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             
             imageView.image = image
+            
             imagePicker.dismiss(animated: true, completion: nil)
+            
+            let visualRecognition = VisualRecognition(apiKey: apiKey, version: version)
+            
+            let imageData = UIImageJPEGRepresentation(image, 0.01)
+            
+            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            
+            let fileURL = documentsURL.appendingPathComponent("tempImage.jpg")
+            
+            try? imageData?.write(to: fileURL, options: [])
+            
+            visualRecognition.classify(imageFile: fileURL, success: { (ClassifiedImages) in
+                print(ClassifiedImages)
+            })
             
         } else {
             
